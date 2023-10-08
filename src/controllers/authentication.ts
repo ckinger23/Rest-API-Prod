80d9ba0f6b65d5196eb81d23f83f5aa48b2bdce4
ckinger <cartersk23@gmail.com>
2023-10-08 17:13:01 -0500

    remove end

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -68,7 +68,7 @@ export const register = async (req: express.Request, res: express.Response) => {
         password: authentication(salt, password),
       },
     });
-    return res.status(200).json(user).end();
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
